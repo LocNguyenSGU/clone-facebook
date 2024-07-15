e34c1843ad7684d5242850978afa64295332542e
LocNguyenSGU <lockbkbang@gmail.com>
2024-07-15 15:42:11 +0700

    style: post

---
+++ src/components/icons/XMarkIcon.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const XMarkIcon = () => {
+  return (
+    <div>
+      <svg
+        viewBox="0 0 20 20"
+        width="20"
+        height="20"
+        fill="currentColor"
+      >
+        <g fill-rule="evenodd" transform="translate(-446 -350)">
+          <g fill-rule="nonzero">
+            <path
+              d="m95.334 195.249 12.917 12.917a1 1 0 0 0 1.415-1.415l-12.917-12.917a1 1 0 0 0-1.415 1.415z"
+              transform="translate(353.5 159)"
+            ></path>
+            <path
+              d="m108.251 193.834-12.917 12.917a1 1 0 0 0 1.415 1.415l12.917-12.917a1 1 0 0 0-1.415-1.415z"
+              transform="translate(353.5 159)"
+            ></path>
+          </g>
+        </g>
+      </svg>
+    </div>
+  );
+};
+
+export default XMarkIcon;
